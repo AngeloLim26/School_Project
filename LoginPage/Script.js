3c4034ac187115d39e0accbd62902c622a2eb5ef
Angelo Lim <Limangelo844@gmail.com>
2025-12-04 18:45:37 +0800

    .

--- LoginPage/Script.js
+++ LoginPage/Script.js
@@ -1,5 +1,5 @@
 const RIGHT_USER = "Zyro";
-const RIGHT_PASS = "12262008";
+const RIGHT_PASS = "123";
 
 const userBox = document.getElementById("username");
 const passBox = document.getElementById("password");
@@ -15,18 +15,22 @@ function checkLogin() {
     infoBox.innerText = "Incorrect username or/and password";
     infoBox.style.display = "block";
     message.innerText = "";
+    infoBox.style.opacity = 1;
   } else if (user === RIGHT_USER && pass === RIGHT_PASS) {
     infoBox.innerText = "";
-    infoBox.style.display = "none";
-    alert("Login successful!");
+    alert("You have Logged in successfully!");
+    infoBox.innerText = "Incorrect username or/and password";
+    infoBox.style.display = "block";
     userBox.value = "";
     passBox.value = "";
+    infoBox.style.opacity = 0;
   } else {
     infoBox.innerText = "Incorrect username or/and password";
     infoBox.style.display = "block";
     message.innerText = "";
     userBox.value = "";
     passBox.value = "";
+    infoBox.style.opacity = 1;
   }
 }
 
